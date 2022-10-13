7562fd920db7a12567a5f27492fe087b1bf1ec1f
gregor murray <gregorian_rants@hotmail.com>
2022-10-13 19:17:56 +0100

    removed the phonebook folder

--- the-phonebook/src/Filter.js
+++
@@ -1,9 +0,0 @@
-function Filter({handleFilterChange}){
-    return (
-        <div>
-            filter shown with <input onChange={handleFilterChange}/>
-        </div>
-    )
-}
-
-export default Filter
